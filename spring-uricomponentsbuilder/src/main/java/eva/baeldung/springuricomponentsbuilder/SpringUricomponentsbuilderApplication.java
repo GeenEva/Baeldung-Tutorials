6b27b610b25481e7dab0dc917de5cbb7228f8bbc
Eva van Geenen <evavangeenen@gmail.com>
2020-09-09 14:16:34 +0200

    first commit

---
+++ spring-uricomponentsbuilder/src/main/java/eva/baeldung/springuricomponentsbuilder/SpringUricomponentsbuilderApplication.java
@@ -0,0 +1,13 @@
+package eva.baeldung.springuricomponentsbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUricomponentsbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUricomponentsbuilderApplication.class, args);
+	}
+
+}
---
+++ spring-uricomponentsbuilder/src/test/java/eva/baeldung/springuricomponentsbuilder/SpringUricomponentsbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package eva.baeldung.springuricomponentsbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUricomponentsbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
